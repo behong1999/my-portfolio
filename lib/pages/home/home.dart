d58981a53548ef9755e7fbf40decf334ce443850
behong1999 <khonhong46632@gmail.com>
2022-11-22 18:00:04 +0200

    remove focus

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -40,94 +40,103 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, ThemeModel themeNotifier, _) => Scaffold(
-        endDrawer: Drawer(
-          backgroundColor:
-              themeNotifier.isDark ? dBackgroundColor : lBackgroundColor,
-          width: 120,
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: ListView.separated(
-                itemBuilder: ((context, index) {
-                  final item = headerItems[index];
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      MouseRegion(
-                        cursor: SystemMouseCursors.click,
-                        onHover: (event) {
-                          setState(() {
-                            textColor = mColor;
-                            currentIndex = headerItems.indexOf(item);
-                          });
-                        },
-                        onExit: (event) {
-                          setState(() {
-                            textColor = null;
-                          });
-                        },
-                        child: AnimatedContainer(
-                          duration: const Duration(
-                            seconds: 1,
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 5),
-                          child: GestureDetector(
-                            onTap: () {
-                              item.onTap();
-                              Timer(
-                                const Duration(milliseconds: 500),
-                                () => Globals.scaffoldKey.currentState!
-                                    .closeEndDrawer(),
-                              );
-                            },
-                            child: Text(
-                              item.title,
-                              style: TextStyle(
-                                color: currentIndex == headerItems.indexOf(item)
-                                    ? textColor
-                                    : null,
-                                fontSize: 13,
-                                fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.focusedChild!.unfocus();
+        }
+      },
+      child: Consumer(
+        builder: (context, ThemeModel themeNotifier, _) => Scaffold(
+          endDrawer: Drawer(
+            backgroundColor:
+                themeNotifier.isDark ? dBackgroundColor : lBackgroundColor,
+            width: 120,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: ListView.separated(
+                  itemBuilder: ((context, index) {
+                    final item = headerItems[index];
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        MouseRegion(
+                          cursor: SystemMouseCursors.click,
+                          onHover: (event) {
+                            setState(() {
+                              textColor = mColor;
+                              currentIndex = headerItems.indexOf(item);
+                            });
+                          },
+                          onExit: (event) {
+                            setState(() {
+                              textColor = null;
+                            });
+                          },
+                          child: AnimatedContainer(
+                            duration: const Duration(
+                              seconds: 1,
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            child: GestureDetector(
+                              onTap: () {
+                                item.onTap();
+                                Timer(
+                                  const Duration(milliseconds: 500),
+                                  () => Globals.scaffoldKey.currentState!
+                                      .closeEndDrawer(),
+                                );
+                              },
+                              child: Text(
+                                item.title,
+                                style: TextStyle(
+                                  color:
+                                      currentIndex == headerItems.indexOf(item)
+                                          ? textColor
+                                          : null,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      )
-                    ],
-                  );
-                }),
-                separatorBuilder: (context, index) {
-                  return const Divider(
-                    thickness: 1,
-                    height: 10,
-                  );
-                },
-                itemCount: headerItems.length,
+                        )
+                      ],
+                    );
+                  }),
+                  separatorBuilder: (context, index) {
+                    return const Divider(
+                      thickness: 1,
+                      height: 10,
+                    );
+                  },
+                  itemCount: headerItems.length,
+                ),
               ),
             ),
           ),
-        ),
-        key: Globals.scaffoldKey,
-        body: NestedScrollView(
-          physics: const NeverScrollableScrollPhysics(),
-          headerSliverBuilder: (context, innerBoxIsScrolled) => [
-            SliverAppBar(
-              toolbarHeight: MediaQuery.of(context).size.height * 0.12,
-              leading: Logo(),
-              leadingWidth: 88,
-              actions: [Header()],
+          key: Globals.scaffoldKey,
+          body: NestedScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            headerSliverBuilder: (context, innerBoxIsScrolled) => [
+              SliverAppBar(
+                toolbarHeight: MediaQuery.of(context).size.height * 0.12,
+                leading: Logo(),
+                leadingWidth: 88,
+                actions: [Header()],
+              ),
+            ],
+            body: ScrollablePositionedList.builder(
+              itemScrollController: itemScrollController,
+              itemPositionsListener: itemPositionsListener,
+              itemCount: componentsList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return componentsList[index];
+              },
             ),
-          ],
-          body: ScrollablePositionedList.builder(
-            itemScrollController: itemScrollController,
-            itemPositionsListener: itemPositionsListener,
-            itemCount: componentsList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return componentsList[index];
-            },
           ),
         ),
       ),
